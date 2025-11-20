84dc60989f4317710dc881b6da8c193712ecd868
Shubham Gaur <shubham.gaur7116@gmail.com>
2025-11-21 02:46:38 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -320,7 +320,7 @@ C6zD8tk=
               <i>Hey</i>, I'm {portfolioData.personal.name}
             </h1>
             <p className="font-body text-sm sm:text-xl lg:text-xl text-gray-600 mb-8 font-normal">
-            Quant • AI x Security • Startups
+            Defi • Quant • AI x Security 
             </p>  
 
             {/* Location and Status */}
